a507c1e648cd10669d1153d54ee06fe93a973f12
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2025-04-26 11:22:08 -0300

    Create User usecase

--- internal/entity/user_entity/user_entity.go
+++ internal/entity/user_entity/user_entity.go
@@ -1,6 +1,16 @@
 package user_entity
 
+import (
+	"context"
+
+	"github.com/pimentafm/auction-go/internal/internal_error"
+)
+
 type User struct {
 	Id   string
 	Name string
+}
+
+type UserRepositoryInterface interface {
+	FindUserById(ctx context.Context, id string) (*User, *internal_error.InternalError)
 }
---
+++ internal/usecase/user_usecase/find_user_usecase.go
@@ -0,0 +1,32 @@
+package user_usecase
+
+import (
+	"context"
+
+	"github.com/pimentafm/auction-go/internal/entity/user_entity"
+	"github.com/pimentafm/auction-go/internal/internal_error"
+)
+
+type UserUseCase struct {
+	UserRepository user_entity.UserRepositoryInterface
+}
+
+type UserOutputDTO struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type UserUserCaseInterface interface {
+	FindUserById(ctx context.Context, id string) (*UserOutputDTO, *internal_error.InternalError)
+}
+
+func (u *UserUseCase) FindUserById(ctx context.Context, id string) (*UserOutputDTO, *internal_error.InternalError) {
+	userEntity, err := u.UserRepository.FindUserById(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &UserOutputDTO{
+		Id:   userEntity.Id,
+		Name: userEntity.Name,
+	}, nil
+}
