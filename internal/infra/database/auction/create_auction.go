62de2aecf8ee6a9548c719eec9e0d0a03bd4b842
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2025-04-26 23:00:52 -0300

    Close Aucion

--- internal/infra/database/auction/create_auction.go
+++ internal/infra/database/auction/create_auction.go
@@ -2,10 +2,13 @@ package auction
 
 import (
 	"context"
+	"os"
+	"time"
 
 	"github.com/pimentafm/auction-go/configuration/logger"
 	"github.com/pimentafm/auction-go/internal/entity/auction_entity"
 	"github.com/pimentafm/auction-go/internal/internal_error"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -45,6 +48,30 @@ func (ar *AuctionRepository) CreateAuction(ctx context.Context, auctionEntity *a
 		logger.Error("Error trying to create auction", err)
 		return internal_error.NewInternalServerError("Error trying to create auction")
 	}
-	return nil
 
+	go func() {
+		select {
+		case <-time.After(getAuctionInterval()):
+			update := bson.M{"$set": bson.M{"status": auction_entity.Completed}}
+			filter := bson.M{"_id": auctionEntity.Id}
+
+			_, err := ar.Collection.UpdateOne(ctx, filter, update)
+			if err != nil {
+				logger.Error("Error trying to update auction status", err)
+				return
+			}
+		}
+	}()
+
+	return nil
+}
+
+func getAuctionInterval() time.Duration {
+	auctionInterval := os.Getenv("AUCTION_INTERVAL")
+	duration, err := time.ParseDuration(auctionInterval)
+	if err != nil {
+		return time.Minute * 5
+	}
+
+	return duration
 }
